7bf32dea30fcd95a147c5f5681fd21b1c868b209
matheusalmeida431 <106746359+matheusalmeida431@users.noreply.github.com>
2024-03-15 08:04:50 -0300

    Update questao01.c

--- questao01.c
+++ questao01.c
@@ -8,7 +8,8 @@ int main() {
     printf("%d, %d, %d, %d\n", p==&i, *p - *q, **&p, 3 - *p/(*q) + 7);
     //para p == &i o resultado será 1, porque se p = i ele retorna true e 1 = true
     //para *p - *q ele retornará os valores que estão contidos nas variaveis para fazer a conta, nesse caso é 3 - 5 = -2
-    
+    //para ** é o ponteiro do ponteiro ou seja p = &3 e &p = p
+    //para 3 - *p/(*q) + 7 ele usa o * que chama os valores declaradas nas variaveis, então aqui ele apenas retornará o resultado da conta
 
     return 0;
 }
