579f38d2636eed8ca6e150b6b9d578f2e7a0bdc8
jbryan0333 <80643318+jbryan0333@users.noreply.github.com>
2024-03-25 11:14:05 -0300

    Create questao12.c

---
+++ questao12.c
@@ -0,0 +1,8 @@
+aloha[2] = value; // Válido
+scanf("%f", &aloha); // Válido
+aloha = "value"; // Invalido
+printf("%f", aloha); // Invalido
+coisas[4][4] = aloha[3]; // Válido
+coisas[5] = aloha; // Invalido
+pf = value; // Invalido
+pf = aloha; // Válido
