8f14624c2998fb79238787b28da37b54018fa2a9
jbryan0333 <80643318+jbryan0333@users.noreply.github.com>
2024-04-03 07:33:28 -0300

    Update questao17.c

--- questao17.c
+++ questao17.c
@@ -1,8 +1,6 @@
+
 #include <stdio.h>
 #include <stdlib.h>
-
-// Protótipo da função de comparação
-typedef int (*Comparacao)(const void *, const void *);
 
 // Função de comparação para ordenação crescente de inteiros
 int comparaInteiros(const void *a, const void *b) {
@@ -11,26 +9,19 @@ int comparaInteiros(const void *a, const void *b) {
     return (*intA - *intB);
 }
 
-// Função de ordenação personalizada para inteiros
-void ordenarInteiros(int *array, int numElementos, Comparacao comp) {
-    for (int i = 0; i < numElementos - 1; i++) {
-        for (int j = 0; j < numElementos - i - 1; j++) {
-            if (comp(&array[j], &array[j + 1]) > 0) {
-                // Troca os elementos se estiverem fora de ordem
-                int temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-            }
-        }
-    }
-}
-
 int main() {
     int array[] = {5, 2, 9, 1, 6};
     int numElementos = sizeof(array) / sizeof(array[0]);
 
-    // Chama a função de ordenação personalizada passando o array, o número de elementos e a função de comparação
-    ordenarInteiros(array, numElementos, comparaInteiros);
+  // Imprime o array ordenado
+  printf("Array desordenada:\n");
+  for (int i = 0; i < numElementos; i++) {
+      printf("%d ", array[i]);
+  }
+  printf("\n");
+  
+    // Usa a função qsort para ordenar o array
+    qsort(array, numElementos, sizeof(int), comparaInteiros);
 
     // Imprime o array ordenado
     printf("Array ordenado:\n");
@@ -41,4 +32,8 @@ int main() {
 
     return 0;
 }
+
+
+
+
 
